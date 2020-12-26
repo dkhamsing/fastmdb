ddefaab9fc797fad3d9e37051ee9a491728638d4
Daniel Khamsing <dkhamsing8@gmail.com>
2020-12-26 09:15:38 -0800

    rename

--- Xcode/FastMDb/TMDB/Media.swift
+++ Xcode/FastMDb/TMDB/Media.swift
@@ -60,9 +60,9 @@ extension Images {
         guard filtered.count > 0 else { return nil }
 
         let items: [Item] = filtered.map {
-            let url1 = Tmdb.backdropImageUrl(path: $0.file_path, size: .original)
-            let url = Tmdb.backdropImageUrl(path: $0.file_path, size: .medium)
-            return Item.ImageItem(url: url1, imageUrl: url)
+            let url = Tmdb.backdropImageUrl(path: $0.file_path, size: .original)
+            let imageUrl = Tmdb.backdropImageUrl(path: $0.file_path, size: .medium)
+            return Item.ImageItem(url: url, imageUrl: imageUrl)
         }
 
         return ItemSection(items: items, display: .collection)
@@ -72,9 +72,9 @@ extension Images {
         guard let profiles = profiles else { return nil }
 
         let items: [Item] = profiles.map {
-            let url1 = Tmdb.castProfileUrl(path: $0.file_path, size: .large)
-            let url = Tmdb.castProfileUrl(path: $0.file_path, size: .medium)
-            return Item.ImageItem(url: url1, imageUrl: url)
+            let url = Tmdb.castProfileUrl(path: $0.file_path, size: .large)
+            let imageUrl = Tmdb.castProfileUrl(path: $0.file_path, size: .medium)
+            return Item.ImageItem(url: url, imageUrl: imageUrl)
         }
 
         return ItemSection(items: items, display: .collection)
